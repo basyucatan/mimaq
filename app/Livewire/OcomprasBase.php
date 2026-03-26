ebc679ded4dcea0ffe8b51fadfff2db1ec671077
Basilio Huchin <basilio_hh@hotmail.com>
2026-03-26 10:42:21 -0600

    controles de navegación en ordenes de compra

--- app/Livewire/OcomprasBase.php
+++ app/Livewire/OcomprasBase.php
@@ -71,7 +71,7 @@ abstract class OcomprasBase extends Component
                     });
             })
             ->orderBy('id', 'desc')
-            ->paginate(4);
+            ->paginate(12);
         return view($nombreVista, [
             'ocompras' => $ocompras,
             'mats' => $this->filtroMats()
